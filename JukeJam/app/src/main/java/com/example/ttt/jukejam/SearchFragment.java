d8226b1951e1afb52d51fde0ce8776bc6b0f2ae4
ryans <ryanszcz@buffalo.edu>
2019-09-16 22:23:42 -0400

    #6 #11 created search page, and accompanying classes

--- JukeJam/app/src/main/java/com/example/ttt/jukejam/SearchFragment.java
+++ JukeJam/app/src/main/java/com/example/ttt/jukejam/SearchFragment.java
@@ -1,7 +1,9 @@
 package com.example.ttt.jukejam;
 
-
+import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
+
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
@@ -10,50 +12,72 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link DJFragment#newInstance} factory method to
+ * Activities that contain this fragment must implement the
+
+ * to handle interaction events.
+ * Use the {@link SearchFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class DJFragment extends Fragment {
+public class SearchFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-    private Song_Request_Adapeter adapter;
     private ListView listView;
+    private Search_Adapter adapter;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
 
-    public DJFragment() {
+
+    public SearchFragment() {
         // Required empty public constructor
     }
 
-
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment SearchFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static SearchFragment newInstance(String param1, String param2) {
+        SearchFragment fragment = new SearchFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        adapter = new Song_Request_Adapeter(getContext(), (ArrayList<SongModel>) dummyData());
+        adapter = new Search_Adapter(getContext(), (ArrayList<SongModel>)dummyData());
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_dj, container, false);
-        listView = v.findViewById(R.id.songRequestsLV);
+        //String roomName = getArguments().getString("Room Name");
+        View v = inflater.inflate(R.layout.fragment_search, container, false);
+        listView = v.findViewById(R.id.searchLV);
         listView.setAdapter(adapter);
         return v;
     }
+
+
     private List<SongModel> dummyData(){
         List<SongModel> retVal = new ArrayList<SongModel>();
         SongModel s = new SongModel("Hey ya!","Outkast",null);
@@ -70,5 +94,4 @@ public class DJFragment extends Fragment {
         Log.d("PartyFragment", "got here: dummyData: ");
         return  retVal;
     }
-
 }
