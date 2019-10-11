4ea94895c2bad8828780cc61594b9e6b1211a632
Sarah <seschwar@buffalo.edu>
2019-10-11 14:02:29 -0400

    build without errors auth tutorial

--- JukeJam/app/src/main/java/com/example/ttt/jukejam/MainActivity.java
+++ JukeJam/app/src/main/java/com/example/ttt/jukejam/MainActivity.java
@@ -1,17 +1,40 @@
 package com.example.ttt.jukejam;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
-
 import android.content.Context;
 import android.os.Bundle;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
 import android.widget.FrameLayout;
+import androidx.fragment.app.FragmentActivity;
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
+import com.spotify.sdk.android.player.Config;
+import com.spotify.sdk.android.player.ConnectionStateCallback;
+import com.spotify.sdk.android.player.Error;
+import com.spotify.sdk.android.player.Player;
+import com.spotify.sdk.android.player.PlayerEvent;
+import com.spotify.sdk.android.player.Spotify;
+import com.spotify.sdk.android.player.SpotifyPlayer;
 
-public class MainActivity extends AppCompatActivity {
-    private FrameLayout fragment_container;
+public class MainActivity extends FragmentActivity implements
+        SpotifyPlayer.NotificationCallback, ConnectionStateCallback
+{
+    // TODO: Replace with your client ID
+    private static final String CLIENT_ID = "4c9fbb8dc6a2473bbd3afff15404170f";
+
+    // TODO: Replace with your redirect URI
+    private static final String REDIRECT_URI = "https://example.com";
+
+    private Player mPlayer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,22 +43,60 @@ public class MainActivity extends AppCompatActivity {
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         ft.replace(R.id.fragment_container,mainFrag);
         ft.commit();
-
     }
 
     @Override
-    public View onCreateView(String name, Context context, AttributeSet attrs) {
-        View rootView = super.onCreateView(name, context, attrs);
-       // setupUI(rootView);
-        return rootView;
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
     }
 
-    public void setupUI(View rootView){
-        //fragment_container = findViewById(R.id.fragment_container);
-        MainFragment mainFrag = new MainFragment();
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.replace(R.id.fragment_container,mainFrag);
-        ft.commit();
-        Log.d("MainActivity", "got here end of setupUI");
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onPlaybackEvent(PlayerEvent playerEvent) {
+        Log.d("MainActivity", "Playback event received: " + playerEvent.name());
+        switch (playerEvent) {
+            // Handle event type as necessary
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onPlaybackError(Error error) {
+        Log.d("MainActivity", "Playback error received: " + error.name());
+        switch (error) {
+            // Handle error type as necessary
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onLoggedIn() {
+        Log.d("MainActivity", "User logged in");
+    }
+
+    @Override
+    public void onLoggedOut() {
+        Log.d("MainActivity", "User logged out");
+    }
+
+    @Override
+    public void onLoginFailed(Error var1) {
+        Log.d("MainActivity", "Login failed");
+    }
+
+    @Override
+    public void onTemporaryError() {
+        Log.d("MainActivity", "Temporary error occurred");
+    }
+
+    @Override
+    public void onConnectionMessage(String message) {
+        Log.d("MainActivity", "Received connection message: " + message);
     }
 }
